ebe58b2466835990bd93329f3fc266b69c2db273
muscleman <musclesonvacation@hotmail.com>
2022-06-25 20:47:06 -0500

    add a little more pizzazz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { AppComponent } from './app.component';
 
 import { ChartModule } from 'angular-highcharts';
 import { MuscleWidgetModule } from './muscleman/muscle-widget.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select'
+import { MatFormFieldModule } from '@angular/material'
+import { MatInputModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,7 +22,13 @@ import { MuscleWidgetModule } from './muscleman/muscle-widget.module';
     AppRoutingModule,
     ChartModule,
     HttpClientModule,
-    MuscleWidgetModule
+    MuscleWidgetModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
